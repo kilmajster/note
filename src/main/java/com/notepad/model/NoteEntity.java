fe55e38ad00566b02053fe6c09a8d53ee490b2a6
Łukasz Włódarczyk <lukasz.wlodarczyk@docflow.pl>
2016-10-05 11:52:50 +0200

    pierwszy commit

---
+++ src/main/java/com/notepad/model/NoteEntity.java
@@ -0,0 +1,62 @@
+package com.notepad.model;
+
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "note")
+public class NoteEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "creation_date", insertable = false, updatable = false)
+    private Date creationDate;
+
+    public NoteEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
